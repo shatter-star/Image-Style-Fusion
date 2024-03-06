e94eee71fd99f8cca8a6d597c7700d95f7d96961
shatter-star <asiddharth_csm206601@mgit.ac.in>
2024-03-06 11:52:41 +0530

    Revamped the codebase for better fault tolerance

--- ml_flow.py
+++ ml_flow.py
@@ -1,12 +1,10 @@
 import mlflow
-import mlflow.pytorch
 import os
 import argparse
 import matplotlib.pyplot as plt
 from NST import neural_style_transfer
-from models.definitions.vgg19 import Vgg19
+from models.definitions.vgg19 import VggModel as Vgg19
 
-# Configuration dictionary
 DEFAULT_CONFIG = {
     "content_weight": 100000.0,
     "style_weight": 30000.0,
@@ -20,66 +18,61 @@ DEFAULT_CONFIG = {
     "img_format": (4, '.jpg')
 }
 
-def run_neural_style_transfer(config):
-    # Instantiate Vgg19 model
-    vgg_model = Vgg19()
-
-    loss_history = []
-    # Set the active experiment
-    experiment_name = "Neural-Style-Transfer"
+def setup_mlflow_experiment(experiment_name):
     mlflow.set_experiment(experiment_name)
-    # Start MLflow run
+
+def log_artifacts(dump_path, config):
+    output_image_path = os.path.join(dump_path, config['content_img_name'].split('.')[0] + '_b1.jpg')
+    mlflow.log_artifact(output_image_path, artifact_path="output_images")
+
+def log_metrics(loss_history):
+    log_interval = 5
+    for i, (total_loss, content_loss, style_loss, tv_loss) in enumerate(loss_history):
+        if i % log_interval == 0:
+            mlflow.log_metric(f'total_loss_{i}', total_loss)
+            mlflow.log_metric(f'content_loss_{i}', content_loss)
+            mlflow.log_metric(f'style_loss_{i}', style_loss)
+            mlflow.log_metric(f'tv_loss_{i}', tv_loss)
+
+def log_model(vgg_model):
+    mlflow.pytorch.log_model(vgg_model, "model")
+    mlflow.pytorch.log_model(vgg_model, "model", registered_model_name="VGG19Model")
+
+def log_loss_curve(loss_history):
+    total_loss_values = [loss[0] for loss in loss_history]
+    style_loss_values = [loss[2] for loss in loss_history]
+    content_loss_values = [loss[1] for loss in loss_history]
+    tv_loss_values = [loss[3] for loss in loss_history]
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(total_loss_values, label='Total Loss')
+    plt.plot(style_loss_values, label='Style Loss')
+    plt.plot(content_loss_values, label='Content Loss')
+    plt.plot(tv_loss_values, label='TV Loss')
+
+    plt.xlabel('Iteration')
+    plt.ylabel('Loss')
+    plt.title('Training Loss Curve')
+    plt.legend()
+
+    plt.yscale('log')
+    plt.savefig('loss_curve.png')
+    mlflow.log_artifact('loss_curve.png')
+
+def run_neural_style_transfer(config):
+    vgg_model = Vgg19()
+    loss_history = []
+
     with mlflow.start_run():
-        # Log parameters
         for key, value in config.items():
             mlflow.log_param(key, value)
 
-        # Run neural style transfer and get dump path
         dump_path, loss_history = neural_style_transfer(config)
-
-        # Log output image
-        output_image_path = os.path.join(dump_path, config['content_img_name'].split('.')[0] + '_b1.jpg')
-        mlflow.log_artifact(output_image_path, artifact_path="output_images")
-
-        # Log metrics
-        log_interval = 5  # Adjust as needed
-        for i, (total_loss, content_loss, style_loss, tv_loss) in enumerate(loss_history):
-            if i % log_interval == 0:
-                mlflow.log_metric(f'total_loss_{i}', total_loss)
-                mlflow.log_metric(f'content_loss_{i}', content_loss)
-                mlflow.log_metric(f'style_loss_{i}', style_loss)
-                mlflow.log_metric(f'tv_loss_{i}', tv_loss)
-
-
-        # Log the whole model
-        mlflow.pytorch.log_model(vgg_model, "model")
-        mlflow.pytorch.log_model(vgg_model, "model", registered_model_name="VGG19Model")
-
-        # Extract loss values from loss_history
-        total_loss_values = [loss[0] for loss in loss_history]
-        style_loss_values = [loss[2] for loss in loss_history]  # Style loss is at index 2
-        content_loss_values = [loss[1] for loss in loss_history]  # Content loss is at index 1
-        tv_loss_values = [loss[3] for loss in loss_history]
-
-        # Plotting the loss curve
-        plt.figure(figsize=(10, 6))  # Adjust figure size if needed
-        plt.plot(total_loss_values, label='Total Loss')
-        plt.plot(style_loss_values, label='Style Loss')
-        plt.plot(content_loss_values, label='Content Loss')
-        plt.plot(tv_loss_values, label='TV Loss')
-
-        # Add labels and title to the plot
-        plt.xlabel('Iteration')
-        plt.ylabel('Loss')
-        plt.title('Training Loss Curve')
-        plt.legend()
-
-        # Save and log the plot
-        plt.yscale('log')
-        plt.savefig('loss_curve.png')
-        mlflow.log_artifact('loss_curve.png')
-
-
+        
+        log_artifacts(dump_path, config)
+        log_metrics(loss_history)
+        log_model(vgg_model)
+        log_loss_curve(loss_history)
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Neural Style Transfer')
@@ -109,6 +102,6 @@ if __name__ == "__main__":
     args = parse_arguments()
     config = vars(args)
     os.makedirs(config['output_img_dir'], exist_ok=True)
-    
-    # Run neural style transfer
+
+    setup_mlflow_experiment("Neural-Style-Transfer")
     run_neural_style_transfer(config)
--- models/definitions/vgg19.py
+++ models/definitions/vgg19.py
@@ -1,60 +1,46 @@
-from collections import namedtuple
 import torch
 from torchvision import models
 
-class Vgg19(torch.nn.Module):
+class VggModel(torch.nn.Module):
     """
-    VGG19 has a total of 19 layers. Out of them, 'conv4_2' is used for content representation,
-    and 'conv1_1', 'conv2_1', 'conv3_1', 'conv4_1', 'conv5_1' are used for style representation.
+    VGG model for neural style transfer.
+    
+    Args:
+        model_type (str): Type of VGG model to use. Currently supports 'vgg19'.
+        requires_grad (bool): Whether to keep model parameters trainable or not.
+        show_progress (bool): Whether to show download progress when loading pretrained model.
     """
-    def __init__(self, requires_grad=False, show_progress=False, use_relu=True):
+
+    def __init__(self, model_type='vgg19', requires_grad=False, show_progress=False):
         super().__init__()
-        vgg_pretrained_features = models.vgg19(pretrained=True, progress=show_progress).features
 
-        self.layer_names = ['relu1_1', 'relu2_1', 'relu3_1', 'relu4_1', 'conv4_2', 'relu5_1']
-        self.offset = 1
-        self.content_feature_maps_index = 4 
-        self.style_feature_maps_indices = list(range(len(self.layer_names)))
-        self.style_feature_maps_indices.remove(4)
-        self.slice1 = torch.nn.Sequential()
-        self.slice2 = torch.nn.Sequential()
-        self.slice3 = torch.nn.Sequential()
-        self.slice4 = torch.nn.Sequential()
-        self.slice5 = torch.nn.Sequential()
-        self.slice6 = torch.nn.Sequential()
+        if model_type == 'vgg19':
+            vgg_pretrained_features = models.vgg19(pretrained=True, progress=show_progress).features
+            self.layer_names = ['relu1_1', 'relu2_1', 'relu3_1', 'relu4_1', 'conv4_2', 'relu5_1']
+            self.content_feature_maps_index = 4 
+            self.style_feature_maps_indices = list(range(len(self.layer_names)))
+            self.style_feature_maps_indices.remove(4)
+            self.offset = 1
+        else:
+            raise ValueError("Unsupported VGG model type. Currently supported: 'vgg19'.")
 
-        for x in range(1+self.offset):
-            self.slice1.add_module(str(x), vgg_pretrained_features[x])
-        for x in range(1+self.offset, 6+self.offset):
-            self.slice2.add_module(str(x), vgg_pretrained_features[x])
-        for x in range(6+self.offset, 11+self.offset):
-            self.slice3.add_module(str(x), vgg_pretrained_features[x])
-        for x in range(11+self.offset, 20+self.offset):
-            self.slice4.add_module(str(x), vgg_pretrained_features[x])
-        for x in range(20+self.offset, 22):
-            self.slice5.add_module(str(x), vgg_pretrained_features[x])
-        for x in range(22, 29++self.offset):
-            self.slice6.add_module(str(x), vgg_pretrained_features[x])
-            
+        self.slices = torch.nn.ModuleList()
+        for x in range(1 + self.offset):
+            self.slices.append(torch.nn.Sequential())
+            self.slices[-1].add_module(str(x), vgg_pretrained_features[x])
+        for idx, (start, end) in enumerate([(1 + self.offset, 6 + self.offset), (6 + self.offset, 11 + self.offset),
+                                             (11 + self.offset, 20 + self.offset), (20 + self.offset, 22)]):
+            self.slices.append(torch.nn.Sequential())
+            for x in range(start, end):
+                self.slices[-1].add_module(str(x), vgg_pretrained_features[x])
+        
         if not requires_grad:
             for param in self.parameters():
                 param.requires_grad = False
 
     def forward(self, x):
-        x = self.slice1(x)
-        layer1_1 = x
-        x = self.slice2(x)
-        layer2_1 = x
-        x = self.slice3(x)
-        layer3_1 = x
-        x = self.slice4(x)
-        layer4_1 = x
-        x = self.slice5(x)
-        conv4_2 = x
-        x = self.slice6(x)
-        layer5_1 = x
-
-        vgg_outputs = namedtuple("VggOutputs", self.layer_names)
-        out = vgg_outputs(layer1_1, layer2_1, layer3_1, layer4_1, conv4_2, layer5_1)
-        
-        return out
+        outputs = []
+        for slice in self.slices:
+            x = slice(x)
+            outputs.append(x)
+        return outputs
